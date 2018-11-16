d7f777e70b9d34c302391b683e3d5d103b031742
Mike Eling <mike.eling97@gmail.com>
2018-11-16 11:06:53 +0100

    📄 Updated documentation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // @flow
 
-/* import YAML from "yamljs";
-import * as fs from 'fs'; */
-
+/**
+ * @class
+ * @public
+ */
 class Littera {
   /**
    * @constructor
-   * @param {object} translations
+   * @param {Object} translations
    */
   constructor(translations = {}) {
     this.translations = translations;
@@ -14,111 +15,11 @@ class Littera {
   }
 
   /**
-   * @name getTranslations
-   * @description Returns loaded translations.
-   * @param {string} language Return translations for a specific language.
-   * @returns {object}
-   */
-  getTranslations = (language = null) => {
-    if (language !== null && !(language instanceof String))
-      throw Error("Language is not a string.");
-
-    if (language) return this.translations[language];
-    return this.translations;
-  };
-
-  /**
-   * @name importTranslations
-   * @description Loads translations.
-   * @param {object} translations
-   */
-  importTranslations = translations => {
-    if (!translations || !(translations instanceof Object))
-      throw Error("Translations is undefined or not an Object.");
-
-    if (this.language === null) this.language = Object.keys(translations)[0];
-    this.translations = translations;
-  };
-
-  /**
-   * @name importJSON
-   * @description Imports translations from a JSON file.
-   * @param {string} path Path to the JSON file.
-   * @param {function} callback
-   * @callback
-   * @deprecated beacause of the "fs not found" problem in React environment.
-   */
-  importJSON = (path, callback) => {
-    const fs = undefined; // Should be imported. [Not importing since its deprecated]
-
-    fs.readFile(require.resolve(path), (err, data) => {
-      if (err) {
-        if (callback) callback(null, err);
-        throw Error(err);
-      } else {
-        const result = JSON.parse(data);
-        this.importTranslations(result);
-        if (callback) callback(result, null);
-      }
-    });
-  };
-  
-  /**
-   * @name importYAML
-   * @description Imports translations from a YAML file.
-   * @param {string} path Path to the YAML file.
-   * @param {function} callback
-   * @callback
-   * @deprecated beacause of the "fs not found" problem in React environment.
-   */
-  importYAML = (path, callback) => {
-    const YAML = undefined; // Should be imported. [Not importing since its deprecated]
-
-    YAML.load(path, (result, err) => {
-      if (err) {
-        if (callback) callback(null, err);
-        throw Error(err);
-      } else {
-        this.importTranslations(result);
-        if (callback) callback(result, null);
-      }
-    });
-  };
-
-
-  /**
-   * @name setLanguage
-   * @description Sets the active language
-   * @param {string} language
-   */
-  setLanguage = language => {
-    if (!language) throw Error("Language is undefined or not a String.");
-
-    this.language = language;
-  };
-
-  /**
-   * @name activeLanguage
-   * @description Returns the active language.
-   * @returns {string}
-   */
-  activeLanguage = () => {
-    if (!this.language) throw "Language is not set.";
-    return this.language;
-  };
-
-  /**
-   * @name getLanguages
-   * @description Returns a list of imported languages.
-   * @returns {array}
-   */
-  getLanguages = () => Object.keys(this.language);
-
-  /**
-   * @name translate
    * @description Returns the right translation for the adequate language.
    * @param {string} key The translation key.
    * @param {string} language (optional)
+   * @example
+   * translate("unique.example", "en_US");
    */
   translate = (key, language = this.language) => {
     if (language === null) throw Error("Language is not set.");
@@ -130,6 +31,56 @@ class Littera {
 
     return this.translations[language][key];
   };
+
+  /**
+   * @description Returns loaded translations.
+   * @param {string} language Return translations for a specific language.
+   * @returns {Object}
+   */
+  getTranslations = (language = null) => {
+    if (language !== null && !(language instanceof String))
+      throw Error("Language is not a string.");
+
+    if (language) return this.translations[language];
+    return this.translations;
+  };
+
+  /**
+   * @description Loads translations.
+   * @param {Object} translations
+   */
+  importTranslations = translations => {
+    if (!translations || !(translations instanceof Object))
+      throw Error("Translations is undefined or not an Object.");
+
+    if (this.language === null) this.language = Object.keys(translations)[0];
+    this.translations = translations;
+  };
+
+  /**
+   * @description Sets the active language
+   * @param {string} language
+   */
+  setLanguage = language => {
+    if (!language) throw Error("Language is undefined or not a String.");
+
+    this.language = language;
+  };
+
+  /**
+   * @description Returns the active language.
+   * @returns {string}
+   */
+  activeLanguage = () => {
+    if (!this.language) throw "Language is not set.";
+    return this.language;
+  };
+
+  /**
+   * @description Returns a list of imported languages.
+   * @returns {Array}
+   */
+  getLanguages = () => Object.keys(this.language);
 }
 
 export default Littera;
